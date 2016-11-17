df4695dea052d70d73060dcce5104b2b3b9b2294
Skylar Wurster <swwurster@gmail.com>
2016-11-17 13:28:55 -0500

    Initial commit with the through tile completed.

---
+++ Assets/Unity Set Builder/Apocalyptic City 2/Ready To Use Pref/Materials Dead/ExtendedFlycam.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class ExtendedFlycam : MonoBehaviour
+{
+ 
+	/*
+	EXTENDED FLYCAM
+		Desi Quintans (CowfaceGames.com), 17 August 2012.
+		Based on FlyThrough.js by Slin (http://wiki.unity3d.com/index.php/FlyThrough), 17 May 2011.
+ 
+	LICENSE
+		Free as in speech, and free as in beer.
+ 
+	FEATURES
+		WASD/Arrows:    Movement
+		          Q:    Climb
+		          E:    Drop
+                      Shift:    Move faster
+                    Control:    Move slower
+                        End:    Toggle cursor locking to screen (you can also press Ctrl+P to toggle play mode on and off).
+	*/
+ 
+
+	public float climbSpeed = 4;
+	public float normalMoveSpeed = 10;
+	public float slowMoveFactor = 0.25f;
+	public float fastMoveFactor = 3;
+ 
+
+ 
+	void Start ()
+	{
+		Screen.lockCursor = true;
+	}
+ 
+	void Update ()
+	{
+ 
+	 	if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift))
+	 	{
+			transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+			transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+	 	}
+	 	else if (Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl))
+	 	{
+			transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+			transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+	 	}
+	 	else
+	 	{
+	 		transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+			transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+	 	}
+ 
+ 
+		if (Input.GetKey (KeyCode.Q)) {transform.position += transform.up * climbSpeed * Time.deltaTime;}
+		if (Input.GetKey (KeyCode.E)) {transform.position -= transform.up * climbSpeed * Time.deltaTime;}
+ 
+		if (Input.GetKeyDown (KeyCode.End))
+		{
+			Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
+		}
+	}
+}
